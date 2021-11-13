6c46c88fb16f2a98090da86f73d12b46c5710ddd
Agustin Blanco <83041211+CodeByNoobie@users.noreply.github.com>
2021-11-13 15:31:13 -0300

    Add files via upload

---
+++ Api Clima/js/app.js
@@ -0,0 +1,121 @@
+const container = document.querySelector('.container');
+const resultado = document.querySelector('#resultado');
+const formulario = document.querySelector('#formulario');
+
+window.addEventListener('load', () => {
+    formulario.addEventListener('submit', buscarClima);
+});
+
+function buscarClima(e){
+    e.preventDefault();
+    
+    const ciudad = document.querySelector('#ciudad').value;
+    const pais = document.querySelector('#pais').value;
+
+    if(ciudad === '' || pais === ''){
+        mostrarError('Ambos campos son obligatorios');
+
+        return;
+    }
+
+    consultarApi(ciudad, pais);
+
+}
+
+function mostrarError(mensaje){
+    console.log(mensaje);
+    const alerta = document.createElement('div');
+    alerta.classList.add('bg-red-100', 'border-red-400', 'text-red-700', 'px-4', 'py-3', 'rounded', 'max-w-md', 'mx-auto', 'mt-6', 'text-center');
+    alerta.innerHTML = ` 
+        <strong class="font-bold">Error!</strong>
+        <span class="block">${mensaje}</span>
+    `;
+
+    container.appendChild(alerta);
+
+    setTimeout(() => {
+       alerta.remove(); 
+    }, 3000);
+
+}
+
+function consultarApi(ciudad, pais){
+    const appId = 'efb227363062a99bcc9a1d45783cdd1b';
+
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+
+    Spiner();
+
+    fetch(url)
+        .then( respuesta => respuesta.json())
+        .then( datos => {
+            limpiarHTML();
+            console.log(datos);
+            if(datos.cod === "404"){
+                mostrarError('Ciudad no encontrada');
+                return;
+            }
+
+            mostrarClima(datos);
+
+        })
+}
+
+function mostrarClima(datos){
+    const { name, main : { temp, temp_max, temp_min } } = datos;
+
+    const centigrados = kelvinACentigrados(temp);
+    const max = kelvinACentigrados(temp_max);
+    const min = kelvinACentigrados(temp_min);
+
+    const nombreCiudad = document.createElement('p');
+    nombreCiudad.textContent = `Clima en ${name}`;
+    nombreCiudad.classList.add('font-bold', 'text-2xl');
+
+    const actual = document.createElement('p');
+    actual.innerHTML = `${centigrados} &#8451;`
+    actual.classList.add('font-bold', 'text-6xl');
+
+    const tempMax = document.createElement('p');
+    tempMax.innerHTML = `Max: ${max} &#8451;`
+    tempMax.classList.add('text-xl');
+
+    const tempMin = document.createElement('p');
+    tempMin.innerHTML = `Min: ${min} &#8451;`
+    tempMin.classList.add('text-xl');
+
+    const resultadoDiv = document.createElement('div');
+    resultadoDiv.classList.add('text-center', 'text-white');
+
+    resultadoDiv.appendChild(nombreCiudad);
+    resultadoDiv.appendChild(actual);
+    resultadoDiv.appendChild(tempMax);
+    resultadoDiv.appendChild(tempMin);
+
+    resultado.appendChild(resultadoDiv);
+}
+
+const kelvinACentigrados = grados => parseInt(grados - 273.15);
+
+
+function limpiarHTML(){
+    while(resultado.firstChild){
+        resultado.removeChild(resultado.firstChild);
+    }
+}
+
+function Spiner(){
+    limpiarHTML();
+    const divSpiner = document.createElement('div');
+    divSpiner.classList.add('spinner');
+
+    divSpiner.innerHTML = `
+    <div class="rect1"></div>
+    <div class="rect2"></div>
+    <div class="rect3"></div>
+    <div class="rect4"></div>
+    <div class="rect5"></div>
+    `;
+
+    resultado.appendChild(divSpiner);
+}
